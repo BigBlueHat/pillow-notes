b8aefb8a5cab2f83caca664aa897c8b1593402fb
BigBlueHat <byoung@bigbluehat.com>
2015-09-24 10:19:39 -0400

    Full CRUD & listing of docs
    Entirely too many changes in a single commit
    but...oh well. :wink:

--- src/main.js
+++ src/main.js
@@ -5,15 +5,68 @@ var Vue = require('vue');
 var db = new PouchDB('pillow-notes');
 window.db = db;
 
+Vue.config.debug = true;
+
 new Vue({
   el: document.body,
   data: {
-    input: '# hello'
+    doc: {
+      _id: '',
+      markdown: ''
+    },
+    ids: []
+  },
+  created: function() {
+    var self = this;
+    self.listDocs();
+  },
+  methods: {
+    listDocs: function() {
+      var self = this;
+      db.allDocs()
+        .then(function(resp) {
+          self.ids = [];
+          for (var i = 0; i < resp.rows.length; i++) {
+            self.ids.push(resp.rows[i].id);
+          }
+        }
+      );
+    },
+    newDoc: function(e) {
+      var self = this;
+      var _id = e.target.value;
+      var doc = {
+        _id: _id,
+        markdown: ''
+      };
+      db.put(doc);
+      // TODO: handle stuff
+      self.doc = doc;
+      e.target.value = '';
+      self.listDocs();
+    },
+    loadDoc: function(e) {
+      e.preventDefault();
+      var self = this;
+      db.get(e.targetVM.$value)
+        .then(function(doc) {
+          self.doc = doc;
+        }
+      );
+    },
+    saveDoc: function(e) {
+      e.preventDefault();
+      db.put(this.doc);
+      // TODO: handle errors and stuff
+    },
+    deleteDoc: function(e) {
+      e.preventDefault();
+      db.remove(this.doc);
+      // TODO: handle errors
+      this.listDocs();
+    }
   },
   filters: {
     marked: marked
-  },
-  components: {
-    'doc-list': require('./doc-list')
   }
 });

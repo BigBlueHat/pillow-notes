dfc465809b22934fee2369c52e83424ffdc866b3
BigBlueHat <byoung@bigbluehat.com>
2015-09-25 13:55:58 -0400

    Expanded doc format; prepping for dates

--- build/bundle.js
+++ build/bundle.js
@@ -24714,14 +24714,18 @@ window.db = db;
 
 Vue.config.debug = true;
 
+var default_doc = {
+  _id: '', // title of the document
+  markdown: '', // markdown
+  created: '', // ISO 8601
+  updated: '' // ISO 8601
+};
+
 window.app = new Vue({
   el: document.body,
   data: {
     new_doc_name: '',
-    doc: {
-      _id: '',
-      markdown: ''
-    },
+    doc: default_doc,
     ids: [],
     remote: {
       url: '',
@@ -24754,30 +24758,23 @@ window.app = new Vue({
     },
     newDoc: function(e) {
       var self = this;
-      var doc = {_id: '', markdown: ''};
+      var doc = default_doc;
       if (self.new_doc_name != '' && self.doc._id == '') {
         // we're making a new doc, but haven't loaded one
-        doc = {
-          _id: self.new_doc_name,
-          markdown: self.doc.markdown
-        };
+        doc._id = self.new_doc_name;
+        doc.markdown = self.doc.markdown;
       } else if (self.new_doc_name == '' && self.doc._id == '') {
         // we're making a new doc, but have only clicked the "+"
-        doc = {
-          _id: (new Date).toISOString(),
-          markdown: self.doc.markdown
-        };
+        doc._id = (new Date).toISOString();
+        doc.markdown = self.doc.markdown;
       } else if (self.doc._id !== '') {
         // there's a previously loaded doc in the editor, so don't save the
         // contents of the editor into the new doc
-        doc = {
-          _id: self.new_doc_name || (new Date).toISOString(),
-          markdown: ''
-        };
+        doc._id = self.new_doc_name || (new Date).toISOString();
+        doc.markdown = '';
       }
-      console.log('doc', doc);
-      console.log('$data', JSON.stringify(self.$data));
-      // save the empty doc immediately
+
+      // save the doc immediately
       db.put(doc)
         .then(function(resp) {
           console.log('resp', resp);
